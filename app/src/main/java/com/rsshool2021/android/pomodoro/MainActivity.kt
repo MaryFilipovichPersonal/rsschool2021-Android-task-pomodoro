85f650a4144421ccd766b236dbc0aeaff34ac13e
MaryFilipovichPersonal <maria0filipovich@gmail.com>
2021-07-20 15:34:39 +0300

    feat: add recycler view fragment feat: make recycler view fragment layout feat: add view holder layout file for timer item

--- app/src/main/java/com/rsshool2021/android/pomodoro/MainActivity.kt
+++ app/src/main/java/com/rsshool2021/android/pomodoro/MainActivity.kt
@@ -2,10 +2,15 @@ package com.rsshool2021.android.pomodoro
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.rsshool2021.android.pomodoro.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
